357475fe720c021b6a87c8b95037acd276369fe2
rupertlv <rob4iks4@gmail.com>
2022-04-22 13:42:49 +0300

    Movement + Cam

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(0,130,20);
+        
+    }
+}
