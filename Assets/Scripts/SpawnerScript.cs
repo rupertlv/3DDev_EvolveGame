5ddd70755237a9c33a62be77f849887d07aca454
rupertlv <rob4iks4@gmail.com>
2022-05-27 23:11:35 +0300

    AI dance dance revolution

--- Assets/Scripts/SpawnerScript.cs
+++ Assets/Scripts/SpawnerScript.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnerScript : MonoBehaviour{
-    public GameObject ItemPrefab;
-    public float Radius = 1;
 
-    void Update(){
-        if (Input.GetKeyDown(KeyCode.Space)) SpawnObjectAtRandom();
+    public GameObject Enemy;
+    public int xPos;
+    public int zPos;
+    public int enemyCount;
+
+    void Start(){
+        StartCoroutine(EnemyDrop());
     }
-    void SpawnObjectAtRandom(){
-        Vector3 randomPos = Random.insideUnitCircle * Radius;
-        Instantiate(ItemPrefab,randomPos,Quaternion.identity);
+    IEnumerator EnemyDrop(){
+        while (enemyCount < 15){
+            xPos = Random.Range(1,100);
+            zPos = Random.Range(1,100);
+            Instantiate(Enemy,new Vector3(xPos, 35, zPos),Quaternion.identity);
+            yield return new WaitForSeconds(0.5f);
+            enemyCount += 1;
+        }
     }
 
 }
