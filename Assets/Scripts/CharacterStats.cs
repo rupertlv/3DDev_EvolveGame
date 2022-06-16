4b7ba56daa8839d34527a401342568f398d56c24
rupertlv <rob4iks4@gmail.com>
2022-06-16 19:28:21 +0300

    New movement

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth {get; private set;}
+    public Stat damage;
+
+    void Awake(){
+        currentHealth = maxHealth;
+    }
+
+    void Update (){
+        if (Input.GetKeyDown(KeyCode.T)){
+            TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage (int damage){
+        currentHealth -= damage;
+        Debug.Log(transform.name + " takes " + damage + " damage.");
+        if (currentHealth <= 0){
+            Die();
+        }
+    }
+    public virtual void Die(){
+
+    }
+}
---
+++ Assets/Scripts/Stat.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stat {
+    public int baseValue;
+    public int GetValue(){
+        return baseValue;
+    }
+}
