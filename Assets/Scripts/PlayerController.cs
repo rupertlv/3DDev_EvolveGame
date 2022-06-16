8d9353e0ed82c03c85e3bba779c0af24b88e413a
rupertlv <rob4iks4@gmail.com>
2022-06-16 20:43:32 +0300

    Idle anim

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour{
 
     public LayerMask movementMask;
+    public Animator anim;
 
     Camera cam;
     PlayerMotor motor;
@@ -17,17 +18,24 @@ public class PlayerController : MonoBehaviour{
         motor = GetComponent<PlayerMotor>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // walking doesn't work
+        // anim.SetFloat("vertical",Input.GetAxis("Vertical"));
+        // anim.SetFloat("horizontal",Input.GetAxis("Horizontal"));
+
         if (Input.GetMouseButtonDown(0)){
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+            
 
             if (Physics.Raycast(ray, out hit, 100, movementMask)){
+
                 motor.MoveToPoint(hit.point);
             }
 
         }
+
+
     }
 }   
