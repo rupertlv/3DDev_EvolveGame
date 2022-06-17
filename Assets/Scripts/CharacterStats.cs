1dd6c14768c3070a591c026234ba9bfc214ad7ef
rupertlv <rob4iks4@gmail.com>
2022-06-17 18:04:24 +0300

    Update CharacterStats.cs

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -3,17 +3,21 @@ using UnityEngine.UI;
 
 public class CharacterStats : MonoBehaviour
 {
+
     public int maxHealth = 100;
     public int currentHealth {get; private set;}
     public int points = 0;
+
     void Awake(){
         currentHealth = maxHealth;
     }
+
     void Update (){
         if (Input.GetKeyDown(KeyCode.T)){
             TakeDamage(10);
         }
     }
+
     public void Heal (int health){
         if (currentHealth == maxHealth){
             Debug.Log(transform.name + " can't heal right now");
@@ -23,6 +27,7 @@ public class CharacterStats : MonoBehaviour
             Debug.Log(transform.name + " heals for " + health);
         }
     }
+
     public void TakeDamage (int damage){
         currentHealth -= damage;
         Debug.Log(transform.name + " takes " + damage + " damage.");
@@ -30,9 +35,11 @@ public class CharacterStats : MonoBehaviour
             Die();
         }
     }
+
     public virtual void Die(){
         Application.LoadLevel(0);
     }
+
     private void OnGUI(){
         GUI.Label(new Rect(10,10,100,20),"Score : " + points);
         GUI.Label(new Rect(10,30,100,20),"Health : " + currentHealth);
